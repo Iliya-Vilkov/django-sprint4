59d71795a82491e7715a49638702937dcc8416e4
Илья Вилков <enot19vilkov@yandex.ru>
2025-06-15 10:27:19 +0300

    correct 2

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 
 from .models import Category, Location, Post
 
+admin.site.empty_value_display = 'Не задано'
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = (
+        'title',
+        'text',
+        'pub_date',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    search_fields = (
+        'title',
+        'description',
+    )
+
+
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Post, PostAdmin)
